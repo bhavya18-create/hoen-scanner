9d8db4f79e9ee345fe0c4147d15aa541b40e5a82
BUGATHA VENKATA BHAVYA <bhavyabugatha18@gmail.com>
2026-02-24 21:10:02 +0530

    Completed Hoen Scanner Search API implementation

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -3,6 +3,10 @@ package com.skyscanner;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
@@ -19,10 +23,33 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     public void initialize(final Bootstrap<HoenScannerConfiguration> bootstrap) {
 
     }
-
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(final HoenScannerConfiguration configuration,
+                    final Environment environment) throws Exception {
 
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Load rental_cars.json
+        List<SearchResult> rentalCars = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class)
+        );
+
+        // Load hotels.json
+        List<SearchResult> hotels = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class)
+        );
+
+        // Combine both lists
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(rentalCars);
+        searchResults.addAll(hotels);
+
+        // Register the /search endpoint
+        environment.jersey().register(new SearchResource(searchResults));
+    }
     }
 
-}
---
+++ src/main/java/com/skyscanner/Search.java
@@ -0,0 +1,13 @@
+package com.skyscanner;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Search {
+
+    @JsonProperty
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+}
